0064a05251b70752b06e89a3ec04ae7c6fe3caef
(zhuxiaofeng) <zhuxiaofeng@zenmen.com>
2017-07-06 18:47:44 +0800

    'init'

---
+++ src/hello.php
@@ -0,0 +1,15 @@
+<?php
+namespace Optionalform;
+/**
+ * Created by PhpStorm.
+ * User: zhuxiaofeng
+ * Date: 2017/7/6
+ * Time: 18:34
+ */
+
+class test{
+
+    function get(){
+        echo  __METHOD__.'test' ;
+    }
+}
